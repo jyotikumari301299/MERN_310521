29ac8aa82ba78fa2c1d0cabb21655427e1ef9aa9
jyotisingh301299 <jyotisingh301299@gmail.com>
2021-05-31 16:03:37 +0530

    connected to mongoDB ATLSA

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = 8000;
 
+const DB = 'mongodb+srv://jyoti:Vande@matram786@cluster0.ikvuv.mongodb.net/mern_300521?retryWrites=true&w=majority';
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(()=>{console.log("CONNECTED TO DB");}).catch((err)=>console.log('not connected to DB',err));
 
 app.get('/',(req,res)=>{
     res.send("hello world from server");
